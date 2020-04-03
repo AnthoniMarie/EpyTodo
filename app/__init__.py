41e846cb3f1937ec1d8dc1ce3af16c4840938e38
Anthoni Marie <contact@anthoni-marie.fr>
2020-04-03 11:58:35 +0200

    [FIX] - Adding db connection

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,18 @@
 
 import os
 from flask import *
+from config import *
+import pymysql as sql
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+connect = sql.connect(host=DATABASE_HOST,
+                      db=DATABASE_NAME,
+                      user=DATABASE_USER,
+                      password=DATABASE_PASS,
+                      unix_socket=DATABASE_SOCK)
+cursor = connect.cursor()
 
 def test():
     return app
