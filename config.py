34892c5700e95434d74b770958c2038463f75589
Anthoni Marie <contact@anthoni-marie.fr>
2020-04-03 12:05:59 +0200

    [FIX] - Updating DB credentials

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ TESTING 	    = True
 DATABASE_NAME 	= "epytodo"
 DATABASE_HOST 	= "127.0.0.1"
 DATABASE_SOCK 	= None
-DATABASE_USER 	= "latoile"
-DATABASE_PASS 	= "4242"
+DATABASE_USER 	= "root"
+DATABASE_PASS 	= ""
