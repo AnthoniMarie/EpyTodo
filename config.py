d0de7411350b5c0f3cb5ccea09234bbfe3989863
Anthoni Marie <contact@anthoni-marie.fr>
2020-04-03 11:59:28 +0200

    [FIX] - Making file more accurate and adding database connection vars

--- config.py
+++ config.py
@@ -6,6 +6,14 @@
 ## config.py
 ##
 
+#SECRET APP KEY
 SECRET_KEY = "weey0y#*$=cr09$-7p!zj96-5636e(sx8+pb*0h4rpv8r$y)8k"
+#DEPLOYMENT GESTURE
 DEBUG	      	= True
 TESTING 	    = True
+#DATABASE CONNECTION GESTURE
+DATABASE_NAME 	= "epytodo"
+DATABASE_HOST 	= "127.0.0.1"
+DATABASE_SOCK 	= None
+DATABASE_USER 	= "latoile"
+DATABASE_PASS 	= "4242"
