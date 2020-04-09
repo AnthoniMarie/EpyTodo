4f3484571760d103fc57bd1c932f0a3e49c5987b
Anthoni Marie <contact@anthoni-marie.fr>
2020-04-09 12:30:03 +0200

    [FIX] - Adding functional get users func

--- app/models.py
+++ app/models.py
@@ -10,21 +10,11 @@ from flask import *
 from config import *
 from app import *
 import pymysql as sql
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy(app)
 
 class User_gesture(object):
-    def user_add(self):
-        request = cursor.execute("SELECT * FROM user");
-
-class Content(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
-    description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Interger, nullable=False)
-
-    def __init__(self, description, gender):
-        self.description = description
-        self.gender = gender
-
-    db.create_all()
+    def get_users_list(data):
+        request = cursor.execute("SELECT * FROM user;")
+        data = cursor.fetchall()
+        return {"username":data}
+    def user_add(data):
+        request = cursor.execute("SELECT * FROM user where username = %s");
