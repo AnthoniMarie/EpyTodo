9109212fdf861117d3423568b6335e3d63df1238
Anthoni Marie <contact@anthoni-marie.fr>
2020-04-11 19:21:02 +0200

    [FIX] - Adding verif user to my controller

--- app/controller.py
+++ app/controller.py
@@ -17,8 +17,10 @@ class UserController(object):
     def user_add(data):
         username = request.form.get('username', None)
         password_ns = request.form.get('password', None)
-        test = UserModel.verif_user_existence(data, username)
-        print(test)
+        if UserModel.verif_user_existence(data, username) == None:
+            print("NEXISTE PAS TA MAMAN LE CHEVAL NOIR DU MONT ROUGE")
+        else:
+            print("IL EXISTE :)")
         if request.method == "POST" and username and password_ns:
             password = hashlib.sha3_256(str(password_ns).encode('utf-8')).hexdigest()
             UserModel.user_add(data, username, password)
