69c9f5517912aa468f745eca7b31a065af031c96
Anthoni Marie <contact@anthoni-marie.fr>
2020-04-11 10:20:22 +0200

    [FIX] - Get value of password, rest to encrypt it

--- app/controller.py
+++ app/controller.py
@@ -16,6 +16,7 @@ import hashlib
 class UserController(object):
     def user_add(data):
         username = request.form.get('username', data)
+        password = request.form.get('password', data)
         #username = request.args['username']
         #print ("Le user est :" + username)
         #print (request.form.getlist(key=db_linkage))
@@ -24,4 +25,4 @@ class UserController(object):
         #username = request.args['username']
         #password = request.args['password']
         return render_template("auth/register.html", title="EPyTodo | Connexion :)",
-                      myContent="Connexion à l'espace membre EPyTodo", data=UserModel.user_add(data, username))
+                      myContent="Connexion à l'espace membre EPyTodo", data=UserModel.user_add(data, username, password))
